7b92a814e8e8ccbacd57d79630ac23b041df2b7d
Joseph Naberhaus <naberhausj@gmail.com>
2019-03-21 17:25:52 -0500

    Completed player connection handshake

--- src/public/javascript/models/PlayerUpdate.ts
+++ src/public/javascript/models/PlayerUpdate.ts
@@ -15,7 +15,7 @@ export class PlayerUpdate {
      * @param id The id of this player
      * @param name THe name of this player
      */
-    constructor(id: v1, name: string) {
+    constructor(id: v1, name: string | undefined) {
         this.id = id;
         this.name = name;
     }
