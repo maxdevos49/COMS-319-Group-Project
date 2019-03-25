b435559c252aa835d476aa2e715403d2604edf11
John Jago <jago@iastate.edu>
2019-03-25 15:56:05 -0500

    Remove unneeded express application

--- test/controllers/GameServer.spec.ts
+++ test/controllers/GameServer.spec.ts
@@ -1,14 +1,12 @@
 import { expect } from 'chai';
 import { Server } from "socket.io";
-import express from "express";
 import http from "http";
 import socketIO from "socket.io";
 
 import { GameServer } from '../../src/controllers/GameServer';
 
 describe('Game server', () => {
-  const app: express.Application = express();
-  const server: http.Server = http.createServer(app);
+  const server: http.Server = http.createServer();
   const gameSocket: Server = socketIO(server);
 
   it('should initialize with an empty list of clients', () => {
