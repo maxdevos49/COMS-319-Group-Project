aa72975721dfb4daadce6f088f817b271d9e80b1
Joseph Naberhaus <naberhausj@gmail.com>
2019-03-26 20:48:21 -0500

    Finished tests

--- src/controllers/GameMatchmaking.ts
+++ src/controllers/GameMatchmaking.ts
@@ -23,7 +23,23 @@ export class GameMatchmaking {
         this.serverSocket = serverSocket;
         // Create the socket for the games endpoint
         this.gamesSocket = serverSocket.of("/games");
-        this.gamesSocket.on("connection", this.connection);
+        this.gamesSocket.on("connection", (socket: Socket) => {
+            // Messages for debugging
+            console.log("A client has connected to the games namespace");
+            socket.on("disconnect", () => {
+                console.log("A client has disconnected to the games namespace");
+            });
+            // Client request list of existing games
+            socket.on("/list", () => {
+                let listToSend: GamesList = new GamesList();
+                // Loop through every game server and add it's id to the list
+                this.games.forEach((game: GameServer) => {
+                    listToSend.gameIds.push(game.serverId);
+                });
+                // Send the list of games to the socket that requested it
+                socket.emit("/list", listToSend);
+            });
+        });
 
         // TODO: Implement smart game begin/end
         this.games.push(new GameServer(serverSocket));
@@ -31,23 +47,4 @@ export class GameMatchmaking {
             "A game server has begun with id: " + this.games[0].serverId
         );
     }
-
-    private connection(socket: Socket): void {
-        // Messages for debugging
-        console.log("A client has connected to the games namespace");
-        socket.on("disconnect", () => {
-            console.log("A client has disconnected to the games namespace");
-        });
-        // Client request list of existing games
-        socket.on("/list", () => {
-            let listToSend: GamesList = new GamesList();
-            // Loop through every game server and add it's id to the list
-            this.games.forEach((game: GameServer) => {
-                listToSend.gameIds.push(game.serverId);
-            });
-            // Send the list of games to the socket that requested it
-            socket.emit("/games/list", listToSend);
-        });
-    }
-
 }
