69c580dc6f45856f345de1adeff6ff30a843f406
Joseph Naberhaus <naberhausj@gmail.com>
2019-03-30 00:42:13 -0500

    Added move update datastructure

---
+++ src/public/javascript/data-sctructures/PlayerMoveUpdateQueue.ts
@@ -0,0 +1,38 @@
+import {PlayerMoveUpdate} from "../models/game/PlayerMoveUpdate";
+
+export class PlayerMoveUpdateQueue {
+    constructor(maxFrameLag: number) {
+
+    }
+
+    /**
+     * Adds a move update to this queue. If frame this move update is for has already occurred (network latency) then
+     * the structure will alias it (pretend that it is for a future update so that it doesn't get ignored). The frame
+     * within the PlayerMoveUpdate will not change
+     *
+     * An update that actually is intended for a frame that is aliased by another one will overwrite the one that is
+     * only aliased to be for that frame (To prevent lag from stacking onto itself).
+     *
+     * If the frame is further behind than the maximum allowed lag then it will be not be saved
+     * @param newUpdate The move update to add to this queue
+     */
+    public addPlayerMoveUpdate(newUpdate: PlayerMoveUpdateQueue): void {
+
+    }
+
+    /**
+     * Pops the update that should be performed next off of the queue. This will be the oldest update on the queue
+     */
+    public popPlayerMoveUpdate(id: string): PlayerMoveUpdate {
+        return null;
+    }
+
+    /**
+     * Increments the current physics frame stored internally by this data structure by one. This should be performed
+     * once the final pop has been performed on this object for the current physics frame.
+     */
+    public incrementFrame(): void {
+
+    };
+
+}
