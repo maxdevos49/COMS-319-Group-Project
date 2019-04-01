e4e8213bb08e03604e1d715851c817a36772a608
Joseph Naberhaus <naberhausj@gmail.com>
2019-03-31 23:28:30 -0500

    Implemented queue to satisfy test cases

--- src/public/javascript/data-sctructures/PositionUpdateQueue.ts
+++ src/public/javascript/data-sctructures/PositionUpdateQueue.ts
@@ -1,6 +1,9 @@
 import {PositionUpdate} from "../models/game/PositionUpdate";
 
 export class PositionUpdateQueue {
+    /**
+     * The map from id to update
+     */
     private updates: Map<string, PositionUpdate>;
 
     constructor() {
@@ -13,14 +16,27 @@ export class PositionUpdateQueue {
      * @param newUpdate The update to add to the queue
      */
     public addUpdate(newUpdate: PositionUpdate): void {
-
+        if (this.updates.has(newUpdate.id)) {
+            // Check if the new update is newer than the old one
+            if (this.updates.get(newUpdate.id).frame < newUpdate.frame) {
+                this.updates.set(newUpdate.id, newUpdate);
+            }
+        } else {
+            this.updates.set(newUpdate.id, newUpdate);
+        }
     }
     /**
      * Pops the most recent new update for the given id or null if no new update exists for it
      * @param id The id of the thing to get the update for
      */
-    public popUpdate(id: string): PositionUpdate {
-        return null;
+    public popUpdate(id: string): PositionUpdate | null {
+        if (this.updates.has(id)) {
+            let tempUpdate: PositionUpdate = this.updates.get(id);
+            this.updates.delete(id);
+            return tempUpdate;
+        } else {
+            return null;
+        }
     }
 
 }
