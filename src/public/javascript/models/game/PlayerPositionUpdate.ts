dbefb7fe21822c6a7f8602afc3d02c7989516ac5
Joseph Naberhaus <naberhausj@gmail.com>
2019-03-31 22:57:53 -0500

    Added model for sending position updates to client

---
+++ src/public/javascript/models/game/PlayerPositionUpdate.ts
@@ -0,0 +1,25 @@
+/**
+ * Defines an update that the server sends to the client informing them where every player in the game is
+ */
+export class PlayerPositionUpdate {
+    /**
+     * The id of the player this move update is for
+     */
+    public id: string;
+    /**
+     * The frame this update is for
+     */
+    public frame: number;
+    /**
+     * The x coordinate of the player
+     */
+    public x: number;
+    /**
+     * The y coordinate of the player
+     */
+    public y: number;
+    /**
+     * The rotation of the player in radians
+     */
+    public facing: number;
+}
