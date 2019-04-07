8a148c14880f6c4c4a7c067fb77bcfce2e093bfe
Joseph Naberhaus <naberhausj@gmail.com>
2019-04-07 15:17:27 -0500

    Added tile width/height to terrain map

--- src/public/javascript/models/game/TerrainMap.ts
+++ src/public/javascript/models/game/TerrainMap.ts
@@ -1,12 +1,20 @@
 export class TerrainMap {
 	/**
-	 * The width of the terrain map
+	 * The width of the terrain map in tiles
 	 */
 	public width: number;
 	/**
-	 * The height of the terrain map
+	 * The height of the terrain map in tiles
 	 */
 	public height: number;
+	/**
+	 * The width of a tile in pixels
+	 */
+	public tileWidth: number;
+	/**
+	 * The height of a tile in pixels
+	 */
+	public tileHeight: number;
 	/**
 	 * The 2d array containing the tile map indices
 	 */
@@ -17,10 +25,14 @@ export class TerrainMap {
 	 * @param width The width of the terrain map
 	 * @param height The height of the terrain map
 	 * @param defaultBlock The default index the terrain map will be initialized with
+	 * @param tileWidth The width of a tile in pixels. Default = 50
+	 * @param tileHeight The height of a tile in pixels. Default = 50
 	 */
-	constructor(width: number, height: number, defaultBlock: number) {
+	constructor(width: number, height: number, defaultBlock: number, tileWidth: number = 50, tileHeight: number = 50) {
 		this.width = width;
 		this.height = height;
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
 		// Initialize a map with the given default block
 		this.map = [];
 		for (let y = 0; y < height; y++) {
