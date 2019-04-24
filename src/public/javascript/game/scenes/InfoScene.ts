2df7a379375e49ce84f2ba02097c93d3c5262ad5
John Jago <jago@iastate.edu>
2019-04-24 13:31:56 -0500

    Refactor the InfoScene so it's easier to expand upon

--- src/public/javascript/game/scenes/InfoScene.ts
+++ src/public/javascript/game/scenes/InfoScene.ts
@@ -1,25 +1,53 @@
 export class InfoScene extends Phaser.Scene {
     /**
-     * Text to be displayed on screen so that the player knows how much HP
-     * is left.
+     * A map of labels and values to be dispalyed on the screen.
      */
-    public hp: string;
+    public infoItems: Map<string, string> = new Map([
+        ["HP", "100"],
+    ]);;
+
+    /**
+     * References to the Phaser text objects which are needed for updating the
+     * text.
+     */
+    public textReferences: Map<string, Phaser.GameObjects.Text> = new Map();
+
+    /**
+     * Configuration for the text.
+     */
+    public config: Object = {
+        font: "48px november",
+        fill: "#fff",
+        backgroundColor: "#000",
+    };
 
     constructor() {
         super({ key: "InfoScene" });
-        this.hp = "100";
     }
 
     /**
-     * Add the info text and listen for updates.
-     *
-     * TODO: Refactor to handle multiple lines of HUD information.
+     * Add the info text to the screen and listen for updates.
      */
     init() {
-        const info = this.add.text(10, 10, "HP: " + this.hp, { font: "48px november", fill: "#000000" });
-        this.scene.get("GameScene").events.on("setHP", (hp: number) => {
-            this.hp = String(hp);
-            info.setText("HP: " + this.hp);
+        let x = 5;
+        let y = 10;
+        this.infoItems.forEach((value: string, label: string) => {
+            const textObj = this.add.text(x, y, `${label}: ${value}`, this.config);
+            this.textReferences.set(label, textObj);
+            y += 50;
         });
+
+        this.setUpListeners();
+    }
+
+    private setUpListeners(): void {
+        this.scene.get("GameScene").events.on("setHP", (hp: number) => {
+            this.updateInfo("HP", String(hp));
+        });
+    }
+
+    private updateInfo(label: string, value: string): void {
+        this.infoItems.set(label, value);
+        this.textReferences.get(label).setText(`${label}: ${value}`);
     }
 }
